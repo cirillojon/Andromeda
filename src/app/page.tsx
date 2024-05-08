f8ff0776aa034a2c5e8953421ba4b3f0e446aa7e
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2024-05-08 16:17:09 -0400

    Init - NextJs 14.2.3 w/ shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center top-10">
+        Home Page
+      </h1>
+    </>
+  );
+}
