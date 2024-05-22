801371035902e16dd088db73a007d2915ef8c9f9
cirillojon <jonathancirillo@outlook.com>
2024-05-22 23:26:45 +0000

    add another example api to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import reqparse, Api, Resource
+
 app = Flask(__name__)
 api = Api(app)
-
-parser = reqparse.RequestParser()
-parser.add_argument('task')
 
 
 class Message(Resource):
     def get(self):
-        return {"message": 'Hello World'}
+        return {"message": "Hello World!"}
 
 
-api.add_resource(Message, '/api/hello')
+class Task(Resource):
+    def get(self):
+        return {"tasks": "List of tasks"}
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    def post(self):
+        data = request.get_json()  # Parses the JSON data
+        if not data or "task" not in data:
+            return {"message": "No task provided"}, 400
+        task_description = data["task"]
+        return {"message": f"Task added: {task_description}"}, 201
+
+
+api.add_resource(Message, "/api/hello")
+api.add_resource(Task, "/api/task")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
