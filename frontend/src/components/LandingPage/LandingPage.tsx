cbb038496e9f80facf6ee7d6dbd5cb9a8bebed1f
ansarijrhit <ansarij@rose-hulman.edu>
2024-05-30 09:37:04 -0700

    Change 3 lines of text

--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -17,23 +17,23 @@ function LandingPage() {
       <MessageComponent /> {/* Flask API Testing */}
       <div className="grid grid-cols-1 grid-rows-1 items-center justify-center max-w-fit h-full md:grid-cols-2 drop-shadow-2xl">
         <div className="flex flex-col w-full mt-24 md:mt-0 mb-14 md:mb-0">
-            <h1 className="header-text">
+            <h1 className="scaling-header-text">
               Empowering{" "}
               <span className="text-gradient text-green-600">You</span>
             </h1>
-            <h1 className="header-text">
+            <h1 className="scaling-header-text">
               To Take{" "}
               <span className="text-gradient text-blue-400">Control</span>
             </h1>
-            <h1 className="header-text">
+            <h1 className="scaling-header-text">
               Of Your{" "}
               <span className="text-gradient text-purple-600">Home</span>
             </h1>
           <div className="flex flex-col items-center text-center">
-            <span className="w-2/3 mt-8">
+            <span className="scaling-text w-2/3 mt-8">
               Tech-driven approach that focuses on delivering a seamless,
               cost-effective, and sustainable home improvement experience for
-              every homeowner
+              every homeowner.
             </span>
           </div>
           <div className="flex flex-col items-center mb-6 md:mb-0">
@@ -44,7 +44,7 @@ function LandingPage() {
         </div>
         <div className="flex flex-col h-full md:h-screen items-center justify-center bg-gradient-to-t from-transparent to-indigo-200 mb-20 md:mb-0">
           <div>
-            <h1 className="header-text blatant">
+            <h1 className="scaling-header-text blatant">
               One Stop Solution
             </h1>
           </div>
