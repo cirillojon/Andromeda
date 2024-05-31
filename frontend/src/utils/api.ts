f584f608a03b00efc421868586996d4e268fbec5
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2024-05-31 11:58:46 -0400

    This was supposed to be with the previous commit

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -6,7 +6,7 @@ export default async function fetchDbUser(currentUser: string)
     //change the base url to the deployed url
     const getDbUserUrl = new URL(
         `/api/user/${currentUser}`,
-        'http://localhost:3000'
+        process.env.NEXT_FRONTEND_BASE_URL
     );
     console.log("Fetching data from " + getDbUserUrl.toString());
     const dbUser: DbUser = await fetch(getDbUserUrl.toString())
@@ -14,7 +14,6 @@ export default async function fetchDbUser(currentUser: string)
         .then((data) => data);
 
     if(dbUser.message === 'User not found'){
-        console.log("USER NOT FOUND IN DATABASE SOMETHING IS WRONG WITH MY USER")
         return null
     }
 
