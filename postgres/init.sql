efbbb652ffe5a013f5ee8b4626eb15f9fe1aea45
asomwaru <asomwaru01@gmail.com>
2024-05-31 02:29:29 -0400

    Might've fixed db connection with custom `db_user` user instead of default postgres admin user

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE home_improvement;
+-- Create the user with a password
 CREATE USER db_user WITH PASSWORD 'postgres';
-GRANT ALL PRIVILEGES ON DATABASE home_improvement TO db_user;
 
-GRANT ALL PRIVILEGES ON SCHEMA public TO db_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO db_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO db_user;
+-- Create the database
+CREATE DATABASE home_improvement OWNER db_user;
+
+GRANT ALL ON SCHEMA public TO db_user;
