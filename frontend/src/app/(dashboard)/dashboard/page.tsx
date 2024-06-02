b5eb14df38bd36e7b8a096dc4aaa9980b11d2fb0
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2024-06-02 19:28:19 -0400

    Cleaned the dashboard code (#111)
    * Removed: extra imports, console.logs, used prettier to format any documents not formatted
    
    * Potential fix for reading values that might not exist
    
    * Fixed scrolling on phones, adjusted padding to fit more content on phones, fixed bottom padding that prevented the bottom project button to be visible on phones

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import HomeDashboard from "@/components/Dashboard/HomeDashboard";
 const page = async () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-screen overflow-y-auto">
       <HomeDashboard />
     </div>
   );
