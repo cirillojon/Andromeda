bad5941639b44e475d49826140b1633e4ee7c0fe
ansarijrhit <ansarij@rose-hulman.edu>
2024-06-03 14:03:34 -0700

    Good looking waitlist page :)

--- frontend/src/components/Onboarding/WaitlistPage/WaitlistPage.tsx
+++ frontend/src/components/Onboarding/WaitlistPage/WaitlistPage.tsx
@@ -20,38 +20,37 @@ const WaitlistPage = () => {
 		
 	return (
 		<div className="grid grid-cols-1 grid-rows-1 items-center justify-center max-w-fit h-full md:grid-cols-2 drop-shadow-2xl mt-16">
-			<div className="flex flex-col h-full md:h-screen items-center justify-center">
-			<div className="flex w-full h-full flex-col items-center">
-				<div className="flex w-full h-full">
+			<div className="flex md:w-full md:h-full flex-col items-left w-0 h-0">
 				<Image
 					src="/assets/hero/florida_better.jpg"
-					alt="House"
+					alt="Florida Map"
 					width={500}
-					height={20}
+					height={200}
 				/>
-				</div>
 			</div>
-			</div>
-			<div className="flex flex-col w-full mt-0 mb-14 md:mb-0">
-				<h1 className="scaling-header-text">
+			<div className="flex flex-col mt-0 mb-14 md:mb-0">
+				<h1 className="scaling-header-text md:mb-10">
 					Join Our Waitlist
 				</h1>
-				<div className="flex items-center">
-					<Label htmlFor="email">Email Address</Label>
+				<span className="scaling-text flex m-auto mb-2 mt-8">Email</span>
+				<div className="flex m-auto md:mb-10">
+					<Input
+						id="email"
+						placeholder="homeimprovement@test.com"
+						type="email"
+						name="email"
+						onInput={checkFormValidity}
+						onEmptied={() => setIsFormValid(false)}
+					/>
+					<button className="ml-2 bg-indigo-400 py-2 px-4 rounded text-white hover:bg-indigo-600">
+						Subscribe
+					</button>
 				</div>
-				<Input
-				id="email"
-				placeholder="homeimprovement@test.com"
-				type="email"
-				name="email"
-				onInput={checkFormValidity}
-				onEmptied={() => setIsFormValid(false)}
-				/>
 			<div className="flex flex-col items-center text-center">
 				<span className="scaling-text w-2/3 mt-8">
 					Romeo is initially launching in the Tampa and Orlando areas. Enter your email above to be alerted when we go live!
 				</span>
-				<span className="scaling-text w-2/3 mt-8">
+				<span className="scaling-text w-2/3 mt-4">
 					The first 100 homeowners to sign-up will receive a promotional offer upon launch.
 				</span>
 			</div>
