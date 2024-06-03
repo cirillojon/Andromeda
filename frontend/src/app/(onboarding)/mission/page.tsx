06562728bddfebe6e7e066c41cf97c3406e9d182
ansarijrhit <ansarij@rose-hulman.edu>
2024-06-03 15:39:48 -0700

    Mission page

---
+++ frontend/src/app/(onboarding)/mission/page.tsx
@@ -0,0 +1,11 @@
+import MissionPage from "@/components/Onboarding/MissionPage/MissionPage";
+
+const page = () => {
+  return (
+    <div>
+      <MissionPage />
+    </div>
+  );
+}
+
+export default page;
