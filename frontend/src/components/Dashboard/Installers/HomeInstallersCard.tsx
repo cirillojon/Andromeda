c4b989a1f0924cb96bf0cb489f18126525b35b9a
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2024-06-04 18:17:46 -0400

    Updated interfaces to current db (#126)
    * Updated interfaces to current db
    
    * financing detail not null
    
    * Installer was broken for null handling

--- frontend/src/components/Dashboard/Installers/HomeInstallersCard.tsx
+++ frontend/src/components/Dashboard/Installers/HomeInstallersCard.tsx
@@ -9,7 +9,7 @@ import {
 import { Installer } from "@/utils/interfaces";
 import Link from "next/link";
 
-const HomeInstallersCard = ({ installer }: { installer: Installer }) => {
+const HomeInstallersCard = ({ installer }: { installer: Installer | null }) => {
   return (
     <Card className="w-full drop-shadow-md">
       <CardHeader>
@@ -22,23 +22,23 @@ const HomeInstallersCard = ({ installer }: { installer: Installer }) => {
         <div className="grid gap-4">
           <div className="flex items-center justify-between">
             <div>
-              <div className="text-xl font-medium mb-1">{installer.name}</div>
+              <div className="text-xl font-medium mb-1">{installer?.name ?? "TBD"}</div>
               <div className="text-gray-500 text-md mb-2">
                 Agent:{" "}
                 <span className="font-bold text-gray-700">
-                  {installer.contact_agent ? installer.contact_agent : "TBD"}
+                  {installer?.contact_agent ? installer.contact_agent : "TBD"}
                 </span>
               </div>
               <div className="text-gray-500 mb-2 text-md">
                 Email:{" "}
                 <span className="font-bold text-gray-700">
-                  {installer.contact_email ? installer.contact_email : "TBD"}
+                  {installer?.contact_email ? installer.contact_email : "TBD"}
                 </span>
               </div>
               <div className="text-gray-500 mb-2 text-md">
                 Phone:{" "}
                 <span className="font-bold text-gray-700">
-                  {installer.contact_phone ? installer.contact_phone : "TBD"}
+                  {installer?.contact_phone ? installer.contact_phone : "TBD"}
                 </span>
               </div>
             </div>
@@ -46,13 +46,20 @@ const HomeInstallersCard = ({ installer }: { installer: Installer }) => {
         </div>
       </CardContent>
       <CardFooter>
+        {installer === null ? (
+          <Link href="#" aria-disabled={true} className="text-gray-300">
+            View Installer
+          </Link>
+        ) : 
+        (
         <Link
-          href={`/dashboard/projects/installers/${installer.id}`}
+          href={`/dashboard/projects/installers/${installer?.id}`}
           className="text-blue-500 hover:underline"
           prefetch={false}
         >
           View installer
         </Link>
+        )}
       </CardFooter>
     </Card>
   );
