bbee6a6dcdc77c20f70422dc3222faa10c3ea5f3
Mehrob <mehrobfarhangmehr@gmail.com>
2024-06-04 23:06:22 -0400

    Created FAQ Page with questions and answers.

---
+++ frontend/src/app/(onboarding)/faq/page.tsx
@@ -0,0 +1,11 @@
+import FAQPage from "@/components/Onboarding/FAQPage/FAQPage";
+
+const page = () => {
+    return (
+        <div>
+            <FAQPage />
+        </div>
+    );
+}
+
+export default page;
