bfce3111cfec547340c5e1e317ae63e891b71b11
ansarijrhit <ansarij@rose-hulman.edu>
2024-06-06 11:20:15 -0700

    Actually address input and such

---
+++ frontend/src/app/(onboarding)/form/page.tsx
@@ -0,0 +1,11 @@
+import AddressPage from "@/components/Onboarding/AddressPage/AddressPage";
+
+const page = () => {
+  return (
+    <div>
+      <AddressPage />
+    </div>
+  );
+}
+
+export default page;
