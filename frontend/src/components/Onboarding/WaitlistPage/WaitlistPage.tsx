137692beae1d9d89071572945afe2d231a92a741
Jon <104590083+cirillojon@users.noreply.github.com>
2024-06-17 23:54:31 -0700

    Update WaitlistPage.tsx

--- frontend/src/components/Onboarding/WaitlistPage/WaitlistPage.tsx
+++ frontend/src/components/Onboarding/WaitlistPage/WaitlistPage.tsx
@@ -1,140 +1,199 @@
 "use client";
-import Image from "next/image";
-import React, { useState } from 'react';
+import React, { useRef, useState, FormEvent } from "react";
+import { Label } from "../../ui/label";
 import { Input } from "../../ui/input";
+import { cn } from "@/lib/utils";
+import { Asterisk } from "lucide-react";
+import { toast } from "sonner";
 
 const WaitlistPage = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    name: '',
-    contactPhone: '',
-    location: '',
-    serviceInterest: '',
-  });
   const [isFormValid, setIsFormValid] = useState(false);
+  const [emailAsterisk, setEmailAsterisk] = useState(true);
+  const [nameAsterisk, setNameAsterisk] = useState(true);
+  const [contactPhoneAsterisk, setContactPhoneAsterisk] = useState(true);
+  const [locationAsterisk, setLocationAsterisk] = useState(true);
+  const [serviceInterestAsterisk, setServiceInterestAsterisk] = useState(true);
 
-  const checkFormValidity = () => {
+  const handleInput = () => {
+    const email = document.getElementById("email") as HTMLInputElement;
+    const name = document.getElementById("name") as HTMLInputElement;
+    const contactPhone = document.getElementById("contactPhone") as HTMLInputElement;
+    const location = document.getElementById("location") as HTMLInputElement;
+    const serviceInterest = document.getElementById("serviceInterest") as HTMLInputElement;
+
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    setIsFormValid(emailRegex.test(formData.email));
+
+    setEmailAsterisk(!emailRegex.test(email.value));
+    setNameAsterisk(!name.value);
+    setContactPhoneAsterisk(!contactPhone.value);
+    setLocationAsterisk(!location.value);
+    setServiceInterestAsterisk(!serviceInterest.value);
+
+    setIsFormValid(
+      emailRegex.test(email.value) &&
+      name.value.length > 0 &&
+      contactPhone.value.length > 0 &&
+      location.value.length > 0 &&
+      serviceInterest.value.length > 0
+    );
   };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-    checkFormValidity();
-  };
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    if (isFormValid) {
+      const formData = new FormData(event.target as HTMLFormElement);
+      const json = Object.fromEntries(formData.entries());
 
-  const handleSubmit = async () => {
-    if (!isFormValid) return;
+      try {
+        const response = await fetch("/api/waitlist", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(json),
+        });
 
-    try {
-      const response = await fetch('/api/waitlist', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        // Handle successful response
-        console.log('Waitlist entry created successfully');
-      } else {
-        // Handle error response
-        console.log('Failed to create waitlist entry');
+        if (response.ok) {
+          toast.success("Waitlist entry created successfully");
+        } else {
+          toast.error("Failed to create waitlist entry");
+        }
+      } catch (error) {
+        console.error("An error occurred while creating the waitlist entry:", error);
+        toast.error("An error occurred while creating the waitlist entry");
       }
-    } catch (error) {
-      console.error('An error occurred while creating the waitlist entry:', error);
     }
   };
 
   return (
-    <div className="grid grid-cols-1 grid-rows-1 items-center justify-center max-w-fit h-full md:grid-cols-2 drop-shadow-2xl mt-16">
-      <div className="flex flex-col mt-0 mb-14 md:mb-0">
-        <h1 className="scaling-header-text md:mb-10">
+    <div className="min-h-screen flex items-center justify-center dark:bg-gray-900">
+      <div className="max-w-md w-full mx-auto rounded-2xl p-4 md:p-8 shadow-input bg-gray-50 dark:bg-black border-2 drop-shadow-lg">
+        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
           Join Our Waitlist
-        </h1>
-        <div className="flex flex-col m-auto mb-2 mt-8">
-          <span className="scaling-text">Email</span>
-          <Input
-            id="email"
-            placeholder="Email Address"
-            type="email"
-            name="email"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="flex flex-col m-auto mb-2">
-          <span className="scaling-text">Name</span>
-          <Input
-            id="name"
-            placeholder="Name"
-            type="text"
-            name="name"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="flex flex-col m-auto mb-2">
-          <span className="scaling-text">Contact Phone</span>
-          <Input
-            id="contactPhone"
-            placeholder="Contact Phone"
-            type="text"
-            name="contactPhone"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="flex flex-col m-auto mb-2">
-          <span className="scaling-text">Location</span>
-          <Input
-            id="location"
-            placeholder="Location"
-            type="text"
-            name="location"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="flex flex-col m-auto mb-2">
-          <span className="scaling-text">Service Interest</span>
-          <Input
-            id="serviceInterest"
-            placeholder="Service Interest"
-            type="text"
-            name="serviceInterest"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="flex m-auto md:mb-10">
+        </h2>
+        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+          Enter your details below to join our waitlist!
+        </p>
+        <form onSubmit={handleSubmit} className="my-8">
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+            <LabelInputContainer>
+              <div className="flex items-center">
+                <Label htmlFor="name">Name</Label>
+                <div hidden={!nameAsterisk}>
+                  <Asterisk className="text-red-500 h-4 w-4" />
+                </div>
+              </div>
+              <Input
+                id="name"
+                placeholder="John Doe"
+                type="text"
+                name="name"
+                onInput={handleInput}
+              />
+            </LabelInputContainer>
+          </div>
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+            <LabelInputContainer>
+              <div className="flex items-center">
+                <Label htmlFor="contactPhone">Contact Phone</Label>
+                <div hidden={!contactPhoneAsterisk}>
+                  <Asterisk className="text-red-500 h-4 w-4" />
+                </div>
+              </div>
+              <Input
+                id="contactPhone"
+                placeholder="(123) 456-7890"
+                type="text"
+                name="contactPhone"
+                onInput={handleInput}
+              />
+            </LabelInputContainer>
+          </div>
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+            <LabelInputContainer>
+              <div className="flex items-center">
+                <Label htmlFor="location">Location</Label>
+                <div hidden={!locationAsterisk}>
+                  <Asterisk className="text-red-500 h-4 w-4" />
+                </div>
+              </div>
+              <Input
+                id="location"
+                placeholder="City, State"
+                type="text"
+                name="location"
+                onInput={handleInput}
+              />
+            </LabelInputContainer>
+          </div>
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+            <LabelInputContainer>
+              <div className="flex items-center">
+                <Label htmlFor="serviceInterest">Service Interest</Label>
+                <div hidden={!serviceInterestAsterisk}>
+                  <Asterisk className="text-red-500 h-4 w-4" />
+                </div>
+              </div>
+              <Input
+                id="serviceInterest"
+                placeholder="Solar Panels, Roofing, etc."
+                type="text"
+                name="serviceInterest"
+                onInput={handleInput}
+              />
+            </LabelInputContainer>
+          </div>
+          <LabelInputContainer className="mb-4">
+            <div className="flex items-center">
+              <Label htmlFor="email">Email Address</Label>
+              <div hidden={!emailAsterisk}>
+                <Asterisk className="text-red-500 h-4 w-4 " />
+              </div>
+            </div>
+            <Input
+              id="email"
+              placeholder="homeimprovement@test.com"
+              type="email"
+              name="email"
+              onInput={handleInput}
+              onEmptied={() => setEmailAsterisk(true)}
+            />
+          </LabelInputContainer>
           <button
+            className=" disabled:opacity-50 disabled:cursor-not-allowed bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            type="submit"
             disabled={!isFormValid}
-            className="ml-2 py-2 px-4 rounded text-white bg-indigo-400 hover:bg-indigo-600"
-            onClick={handleSubmit}
           >
-            Subscribe
+            Subscribe &rarr;
+            <BottomGradient />
           </button>
-        </div>
-        <div className="flex flex-col items-center text-center">
-          <span className="scaling-text w-2/3 mt-8">
-            Romeo is initially launching in the Tampa and Orlando areas. Enter your email above to be alerted when we go live!
-          </span>
-          <span className="scaling-text w-2/3 mt-4">
-            The first 100 homeowners to sign-up will receive a promotional offer upon launch.
-          </span>
-        </div>
-      </div>
-      <div className="flex md:w-full md:h-full flex-col items-end w-0 h-0">
-        <Image
-          src="/assets/hero/florida_better.jpg"
-          alt="Florida Map"
-          width={500}
-          height={200}
-        />
+        </form>
       </div>
     </div>
   );
 };
 
 export default WaitlistPage;
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
